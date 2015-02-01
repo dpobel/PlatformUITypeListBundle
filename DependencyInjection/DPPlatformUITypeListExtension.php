7a7d8b89603388accf23d85ab98e7b59dd45d0aa
Damien Pobel <dp@ez.no>
2015-02-01 15:00:36 +0100

    Prepared the bundle to extend PlatformUI

--- DependencyInjection/DPPlatformUITypeListExtension.php
+++ DependencyInjection/DPPlatformUITypeListExtension.php
@@ -5,24 +5,49 @@ namespace DP\PlatformUITypeListBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\SiteAccessAware\ConfigurationProcessor;
+use Symfony\Component\Config\Resource\FileResource;
 
-/**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
-class DPPlatformUITypeListExtension extends Extension
+class DPPlatformUITypeListExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load( array $configs, ContainerBuilder $container )
     {
         $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        $config = $this->processConfiguration( $configuration, $configs );
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new Loader\YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
+        $loader->load( 'services.yml' );
+
+        $processor = new ConfigurationProcessor( $container, 'dp_platformuitypelist' );
+    }
+
+    public function prepend( ContainerBuilder $container )
+    {
+        $container->prependExtensionConfig( 'assetic', array( 'bundles' => array( 'DPPlatformUITypeListBundle' ) ) );
+
+        $this->prependYui( $container );
+        $this->prependCss( $container );
+    }
+
+    private function prependYui( ContainerBuilder $container )
+    {
+        $yuiConfigFile = __DIR__ . '/../Resources/config/yui.yml';
+        $config = Yaml::parse( file_get_contents( $yuiConfigFile ) );
+        $container->prependExtensionConfig( 'ez_platformui', $config );
+        $container->addResource( new FileResource( $yuiConfigFile ) );
+    }
+
+    private function prependCss( ContainerBuilder $container )
+    {
+        $cssConfigFile = __DIR__ . '/../Resources/config/css.yml';
+        $config = Yaml::parse( file_get_contents( $cssConfigFile ) );
+        $container->prependExtensionConfig( 'ez_platformui', $config );
+        $container->addResource( new FileResource( $cssConfigFile ) );
     }
 }
